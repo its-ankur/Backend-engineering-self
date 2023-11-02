86c01a42295a7c071889ec314db981d814719a7e
its-ankur <ankuratul2007@gmail.com>
2023-11-02 10:59:59 +0530

    Mongoose new work

--- Mongoose/server.js
+++ Mongoose/server.js
@@ -1,12 +1,39 @@
 const mongoose = require("mongoose");
 const User = require("./user");
 mongoose.connect("mongodb://127.0.0.1/testdb");
-async function run() {
-    const user=await User.create({"name":"Ankur","age":20});
-    user.name="atul";
-    await user.save()
-    // const user = new User({ name: "ankur", age: 20 });
-    // await user.save();
-    console.log(user);
-}
+
 run();
+async function run() {
+   // User.findById.save();
+    try{
+        const user=await User.where("age")
+        .gte(20)
+        .where("name")
+        .equals("Ankur")
+        .limit(1)
+        .select("age");
+        console.log(user);
+        // const user = await User.create({
+        //     name: "Atul",
+        //     age: 20,
+        //     email:"ankur@gmail.com",
+        //     hobbies:["Cricket","Football"],
+        //     address:{
+        //         street:"xyz",
+        //         city:"baddi"
+        //     }
+        // });
+        // user.createdAt="2020-10-10";
+        // await user.save();
+        // console.log(user);
+    }
+    catch(e){
+        console.log(e.message);
+    }
+    // user.name="atul";
+    // await user.save()
+    // // const user = new User({ name: "ankur", age: 20 });
+    // // await user.save();
+    // console.log(user);
+
+}
--- Mongoose/user.js
+++ Mongoose/user.js
@@ -1,16 +1,43 @@
 const mongoose=require('mongoose');
+
+const addressSchema=new mongoose.Schema({
+    streeet:String,
+    city:String
+})
 const userSchema=new mongoose.Schema({
     name:String,
-    age:Number,
-    email:String,
-    createdDate:Date,
-    updatedDate:Date,
-    bestFriend:mongoose.SchemaTypes.ObjectId,
+    age:{
+        type:Number,
+        min:1,
+        max:100,
+        validate:{
+            validator:function(value){
+                return value%2==0;
+            },
+            message:"Age must be even"
+        }
+    },
+    email:{
+        type:String,
+        minlength:10,
+        required:true,
+        lowercase:true,
+    },
+    createdAt:{
+        type:Date,
+        immutable:true,
+        default:()=>Date.now(),
+    },
+    updatedAt:{
+        type:Date,
+        default:()=>Date.now(),
+    },
+    bestFriend:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:"User",
+    },
     hobbies:[String],
-    address:{
-        street:String,
-        city:String
-    }
+    address:addressSchema
 })
 
 module.exports=mongoose.model("User",userSchema);
